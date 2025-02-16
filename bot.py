f6cfab22ad82eb982467e845592328be3d316edd
Chel2387 <tntrsvch@gmail.com>
2025-02-16 19:20:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,19 @@
 import telebot
+import os
 from bot_logic import gen_pass
+
+text_messages = {
+    'info':
+        u'Привет, Я твой бот Бро,\n'
+        u'У меня есть кучу команд.\n'
+        u'Вот они: '
+        u'start, hello, bye, password, heh, help/info',
+}
+
+
+# Замени 'TOKEN' на токен твоего бота
+# Этот токен ты получаешь от BotFather, чтобы бот мог работать
+bot = telebot.TeleBot("TOKEN")
     
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -12,7 +26,22 @@ def send_hello(message):
 @bot.message_handler(commands=['bye'])
 def send_bye(message):
     bot.reply_to(message, "Пока! Удачи!")
-  
+
+@bot.message_handler(commands=['password'])
+def send_bye(gen_pass):
+    bot.reply_to(gen_pass())
+
+# Обработчик команды '/heh'
+@bot.message_handler(commands=['heh'])
+def send_heh(message):
+    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
+    bot.reply_to(message, "he" * count_heh)
+    
+@bot.message_handler(commands=['info', 'help'])
+def on_info(message):
+    bot.reply_to(message, text_messages['info'])
+
+    
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
